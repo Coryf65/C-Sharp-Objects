7a8fa8376699ed44c86e328f429a9a84ecae669e
Cory <Coryf65@gmail.com>
2018-01-24 21:02:51 -0600

    Added Custom Exceptions with general exceptions and a few specific ones

--- C-Sharp-Objects/Game.cs
+++ C-Sharp-Objects/Game.cs
@@ -7,14 +7,27 @@ namespace TreehouseDefense
         public static void Main()
         {
             Map map = new Map(8, 5);
-            
+
             try
             {
                 MapLocation mapLocation = new MapLocation(20, 20, map);
             }
-            catch(Exception ex)
+
+            //To catch multiple exceptions the general MUST be LAST
+            //and the more specific before it
+            catch (OutOfBoundsException ex)//See if a range it not within the MAP
             {
                 Console.WriteLine(ex.Message);
+            }
+
+            catch (TreehouseDefenseException)
+            {
+                Console.WriteLine("Unhandled TreehouseDefenseException");
+            }
+
+            catch (Exception)//will catch all exceptions
+            {
+                Console.WriteLine("Unhandled Exception");
             }
         }
     }
