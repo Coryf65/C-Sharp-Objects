1f32f738a1281cd7301747b11737e6af3aa99303
Cory <Coryf65@gmail.com>
2018-01-25 18:44:52 -0600

    Added Encapsulation to the Path Class, and created an array for the path

--- C-Sharp-Objects/Game.cs
+++ C-Sharp-Objects/Game.cs
@@ -10,7 +10,18 @@ namespace TreehouseDefense
 
             try
             {
-                MapLocation mapLocation = new MapLocation(20, 20, map);
+                MapLocation[] path = {
+                    new MapLocation(0, 2, map),
+                    new MapLocation(1, 2, map),
+                    new MapLocation(2, 2, map),
+                    new MapLocation(3, 2, map),
+                    new MapLocation(4, 2, map),
+                    new MapLocation(5, 2, map),
+                    new MapLocation(6, 2, map),
+                    new MapLocation(7, 2, map)
+
+                };
+                    
             }
 
             //To catch multiple exceptions the general MUST be LAST
--- C-Sharp-Objects/Path.cs
+++ C-Sharp-Objects/Path.cs
@@ -2,6 +2,33 @@ namespace TreehouseDefense
 {
     class Path
     {
-        
+        private readonly MapLoacation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+
+            return (pathStep < _path.Length) ? (_path[pathStep]) : null; // Using Turinary operator in replace of the If Else Block
+
+
+
+            // Using Turinary operator in replace of the If Else Block BELOW...
+            //if (pathStep < _path.Length)
+            //{
+
+            //    return _path[pathStep];
+
+            //}
+            //else
+            //{
+            //    return null;
+            //}
+        }
+
     }
 }
