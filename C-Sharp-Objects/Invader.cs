517178bd550552fc8d5c51358c3d329b2a22a58b
Cory <Coryf65@gmail.com>
2018-01-27 17:46:17 -0600

    Created a Path start point for the invaders

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -1,11 +1,26 @@
 namespace TreehouseDefense
 {
     class Invader
-    {      
+    {
+
+        private readonly Path _path;
+        private int _pathStep = 0;
 
         // Shorthanded way vs the whole other way
         public MapLocation Location { get; private set; }
         
+        public Invader( Path path )
+        {
+
+            Location = path.GetLocationAt(_pathStep);
+        }
+
+        public void Move()
+        {
+            _pathStep += 1;
+            Location = _path.GetLocationAt(_pathStep);
+        }
+
 
     }
 }
