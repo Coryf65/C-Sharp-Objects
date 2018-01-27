c9ed2f636624594dc06dcaf9d9553ed1e7396d8f
Cory <Coryf65@gmail.com>
2018-01-26 21:30:34 -0600

    Creating a getter and setter

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -2,6 +2,17 @@ namespace TreehouseDefense
 {
     class Invader
     {
+
+        private MapLocation _location;
+
+        public MapLoaction GetLocation()
+        {
+            return Loaction;
+        }
         
+        public void SetLocation(MapLocation value)
+        {
+            _locaiton = value;
+        }
     }
 }
