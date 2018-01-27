391915a657bee395f1fd55b1c1e5cb283398bd33
Cory <Coryf65@gmail.com>
2018-01-27 17:41:57 -0600

    Got rid of the 'private MapLocation _location;' did not need it

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -1,12 +1,11 @@
 namespace TreehouseDefense
 {
     class Invader
-    {
-
-        private MapLocation _location;
+    {      
 
         // Shorthanded way vs the whole other way
         public MapLocation Location { get; private set; }
         
+
     }
 }
