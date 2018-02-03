2ce687fefbab9663f7b3a8f478139a2699321545
Cory <Coryf65@gmail.com>
2018-02-03 10:34:28 -0600

    Notes about how to improve this class. by using the DRY principals

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -11,14 +11,14 @@ namespace TreehouseDefense
         
         public Invader( Path path )
         {
-
-            Location = path.GetLocationAt(_pathStep);
+            _path = path;
+            Location = path.GetLocationAt(_pathStep); // DRY (Don't Repeat Yourself)
         }
 
         public void Move()
         {
-            _pathStep += 1;
-            Location = _path.GetLocationAt(_pathStep);
+            _pathStep += 1; // if we want to update the code we have to remember to change this field BUT instead we are going to change it up
+            Location = _path.GetLocationAt(_pathStep); // DRY (Don't Repeat Yourself) these two are similar
         }
 
 
