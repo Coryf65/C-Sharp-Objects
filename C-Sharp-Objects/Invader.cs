feddbe442e4700810cef9bf88b53caed91dc2ec9
Cory <Coryf65@gmail.com>
2018-02-03 11:16:32 -0600

    Solution to challenge

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -4,30 +4,30 @@ namespace TreehouseDefense
     {
 
         /**
-         *  Challenger Approaching !!!!!!!
-         * Give the Invader's Health, be disabled, health remaining 
-         * hit and decrement helath
-         * **/
+         *  Jeremy's Solution
+         **/
 
         private int _Health = 10;
         private readonly Path _path;
-        private int _pathStep = 0;
-        private string _missShot = "0";
 
         public MapLocation Location => _pathStep.GetLocationAt(_pathStep);
 
+        // property
+        public int Health { get; set; } = 2;
 
         public Invader( Path path )
         {
             _path = path;
+
         
         }
 
         public void Move() => _pathStep += 1;
 
-        // when a tower hits the invader
-        public void HitSuccess() => _Health -= 1;
-
-        public string HitFailed => _missShot;
+        // a way to decrease health
+        public void DecreaseHealth(int factor)
+        {
+            Health -= factor;
+        }
     }
 }
