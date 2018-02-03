454c318b29c7bdb3ab85a4fe20fe349a1054cb15
Cory <Coryf65@gmail.com>
2018-02-03 10:36:20 -0600

    Setting up for a Computed Property

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -7,18 +7,18 @@ namespace TreehouseDefense
         private int _pathStep = 0;
 
         // Shorthanded way vs the whole other way
-        public MapLocation Location { get; private set; }
+        public MapLocation Location { get; private set; } // change into a computed property, so the Getter is COMPUTED each time it's called
         
         public Invader( Path path )
         {
             _path = path;
-            Location = path.GetLocationAt(_pathStep); // DRY (Don't Repeat Yourself)
+            Location = path.GetLocationAt(_pathStep); 
         }
 
         public void Move()
         {
-            _pathStep += 1; // if we want to update the code we have to remember to change this field BUT instead we are going to change it up
-            Location = _path.GetLocationAt(_pathStep); // DRY (Don't Repeat Yourself) these two are similar
+            _pathStep += 1; 
+            Location = _path.GetLocationAt(_pathStep); 
         }
 
 
