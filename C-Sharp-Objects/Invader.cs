b64e10d2e438e3c10ebbacc389823b8a23bfa9e6
Cory <Coryf65@gmail.com>
2018-02-03 10:54:58 -0600

    Changed the Invader class into a Computed Property

--- C-Sharp-Objects/Invader.cs
+++ C-Sharp-Objects/Invader.cs
@@ -6,19 +6,34 @@ namespace TreehouseDefense
         private readonly Path _path;
         private int _pathStep = 0;
 
-        // Shorthanded way vs the whole other way
-        public MapLocation Location { get; private set; } // change into a computed property, so the Getter is COMPUTED each time it's called
-        
+        //NOW a computed Property ...
+        // replaces the property that has a field, a getter, and a setter
+        // the user of this property does not need to know that the location 
+        //of this property is computed from the _path and the _pathstep field
+        // all they need to know is that they can type invader.location and 
+        // get the location of the invader on the map
+
+            // Properties should do simple things LIKLE getting and setting data
+            
+        public MapLocation Location
+        {
+            get
+            {
+                return _path.GetLocationAt(_pathStep);
+            }
+
+        }
+
         public Invader( Path path )
         {
             _path = path;
-            Location = path.GetLocationAt(_pathStep); 
+        
         }
 
         public void Move()
         {
             _pathStep += 1; 
-            Location = _path.GetLocationAt(_pathStep); 
+         
         }
 
 
