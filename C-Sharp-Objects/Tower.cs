09d3c56348f16c35eb2625a26f6bb56f6e2eeb4d
Cory <Coryf65@gmail.com>
2018-06-16 20:30:57 -0500

    + fireOnInvaders Method + setting up a Tower Class
    as well as learning some differences between different loops

--- C-Sharp-Objects/Tower.cs
+++ C-Sharp-Objects/Tower.cs
@@ -2,6 +2,45 @@ namespace C_Sharp_Objects
 {
     class Tower
     {
+
+        private readonly MapLocation _loaction;
+
+        public Tower(MapLocation location)
+        {
+            _loaction = location;
+        }
         
+        public void FireOnInvaders(Invader[] invaders)
+        {
+
+            /*
+             * Using a While vs a For loop
+             */
+
+            //int index = 0;
+
+            //while (index < invaders.Length)
+            //{
+            //    Invader invader = invaders[index];
+            //    // Do stuff with the invader
+
+            //    index++;
+            //}
+
+
+            // similar to a while loop
+            //for (int i = 0; i < invaders.Length; i++)
+            //{
+            //    Invader invader = invaders[i];
+            //    // Do Stuff
+            //}
+
+            //loop through each item in the collection
+            foreach (Invader invader in invaders)
+            {
+                // Do stuff
+            }
+        }
+
     }
 }
