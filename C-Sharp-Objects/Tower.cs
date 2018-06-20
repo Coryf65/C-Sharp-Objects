c44b43208ee0ffecdda4f6fc6c90c48062f4545b
Cory <Coryf65@gmail.com>
2018-06-19 21:30:06 -0500

    Changed the int values into 'const' so that they are clearly understood why and what they are for.

--- C-Sharp-Objects/Tower.cs
+++ C-Sharp-Objects/Tower.cs
@@ -2,7 +2,9 @@ namespace C_Sharp_Objects
 {
     class Tower
     {
-
+        //Constant where you cannot change the value after declaration
+        private const int _RANGE = 1;
+        private const int _POWER = 1;
         private readonly MapLocation _loaction;
 
         public Tower(MapLocation location)
@@ -16,11 +18,12 @@ namespace C_Sharp_Objects
             //loop through each item in the collection
             foreach (Invader invader in invaders)
             {
+               
                 // Do stuff
                 // 1 is the grid size
-                if (invader.IsActive && _loaction.InRangeOf(invader.Location, 1))
+                if (invader.IsActive && _loaction.InRangeOf(invader.Location, _RANGE))
                 {
-                    invader.DecreaseHealth(1);
+                    invader.DecreaseHealth(_POWER);
                     break;
                 }
             }
