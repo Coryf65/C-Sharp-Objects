9f8895641c04c50ebfe775cb48ac1c7154659e0f
Cory <Coryf65@gmail.com>
2018-09-20 19:10:52 -0500

    Created more interfaces so that we can inherit multiple methods.

--- C-Sharp-Objects/IInvader.cs
+++ C-Sharp-Objects/IInvader.cs
@@ -2,31 +2,32 @@
 
 namespace C_Sharp_Objects
 {
-    interface IInvader
+
+
+    interface IMappable
     {
-
-        // New interface
-        // they don't have constructors, or implmentations,
-        // they also only define public members
-
-        // computed property, only needs a getter
         MapLocation Location { get; }
+    }
+
+    internal interface IMovable
+    {
+        void Move();
+    }
+
+    interface IInvader : IMappable, IMovable
+    {          
         
-        //Computed Proerty true if the invader reaches the end of the path
         bool HasScored { get; }
         
-        // property
         int Health { get; }
 
         bool IsNeutralized { get; }
 
         bool IsActive { get; }
 
-        // Using a Virtual Property
-        void Move();
-
         void DecreaseHealth(int factor);
 
 
     }
+  
 }
